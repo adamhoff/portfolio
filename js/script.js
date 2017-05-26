c59d61ae04d94fa6f0b86882450074983c6e695b
Adam Hoff <swimmerdude79@comcast.net>
2017-05-26 14:25:00 -0400

    continuing updates

--- js/script.js
+++ js/script.js
@@ -16,29 +16,26 @@ $(document).ready(function() {
   });
 
   $(window).scroll(function() {
+    if ($(document).scrollTop() < 200) {
+      $(".navbar-brand").fadeOut(1200);
+    }
     if ($(document).scrollTop() > 200 && $(document).scrollTop() < 1600) {
       $(".about-container, .navbar-brand").fadeIn(1200);
     }
-    else {
-      $(".about-container").fadeOut(1200);
-    }
+    // else {
+    //   $(".about-container").fadeOut(1200);
+    // }
     if ($(document).scrollTop() > 1000 && $(document).scrollTop() < 2400)  {
-      $(".skills-container, .navbar-brand").fadeIn(1200);
-    }
-    else {
-      $(".skills-container").fadeOut(1200);
-    }
-    if ($(document).scrollTop() > 1800 && $(document).scrollTop() < 3200)  {
       $(".project1-container, .navbar-brand").fadeIn(1200);
     }
-    else {
-      $(".project1-container").fadeOut(1200);
-    }
-    if ($(document).scrollTop() > 2600 && $(document).scrollTop() < 4000) {
+    // else {
+    //   $(".project1-container").fadeOut(1200);
+    // }
+    if ($(document).scrollTop() > 1800 && $(document).scrollTop() < 3200) {
       $(".project2-container, .navbar-brand").fadeIn(1200);
     }
-    else {
-      $(".project2-container").fadeOut(1200);
-    }
+    // else {
+    //   $(".project2-container").fadeOut(1200);
+    // }
   });
 });
